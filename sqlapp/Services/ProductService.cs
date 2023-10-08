f8ef5c4b9d857b979ab9b5dfa17bd45fec7c7351
max32aami <142899508+max32aami@users.noreply.github.com>
2023-10-08 12:37:44 +0500

    new committ

--- sqlapp/Services/ProductService.cs
+++ sqlapp/Services/ProductService.cs
@@ -5,33 +5,45 @@ namespace sqlapp.Services
 {
 
     // This service will interact with our Product data in the SQL database
-    public class ProductService
+    public class ProductService : IProductService
     {
-        private static string db_source = "tcp:maxsql.database.windows.net,1433";
-        private static string db_user = "sqladmin";
-        private static string db_password = "Netsolpk1!";
-        private static string db_database = "appdb";
-
+        private readonly IConfiguration _configuration;
+        public ProductService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         private SqlConnection GetConnection()
         {
-            
+            var builder = WebApplication.CreateBuilder();
+            var app = builder.Build();
+            if(app.Environment.IsDevelopment())
+            {
+                return GetConnectionLocal();
+            }
+            return new SqlConnection(_configuration.GetConnectionString("SQLConnection"));
+        }
+
+        private SqlConnection GetConnectionLocal()
+        {
+
             var _builder = new SqlConnectionStringBuilder();
-            _builder.DataSource = db_source;
-            _builder.UserID = db_user;
-            _builder.Password = db_password;
-            _builder.InitialCatalog = db_database;
+            _builder.DataSource = "tcp:maxsql.database.windows.net,1433";
+            _builder.UserID = "sqladmin";
+            _builder.Password = "Netsolpk1!";
+            _builder.InitialCatalog = "appdb";
             return new SqlConnection(_builder.ConnectionString);
         }
+
         public List<Product> GetProducts()
         {
             List<Product> _product_lst = new List<Product>();
             string _statement = "SELECT ProductID,ProductName,Quantity from Products";
             SqlConnection _connection = GetConnection();
-            
+
             _connection.Open();
-            
+
             SqlCommand _sqlcommand = new SqlCommand(_statement, _connection);
-            
+
             using (SqlDataReader _reader = _sqlcommand.ExecuteReader())
             {
                 while (_reader.Read())
